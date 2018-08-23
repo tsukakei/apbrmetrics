b278fcc58b3bc3047e405802389a965f1ba836bc
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-08-23 12:03:37 +0900

    add metrics about free-throw and rebounds

--- apbrmetrics/metrics.py
+++ apbrmetrics/metrics.py
@@ -107,3 +107,74 @@ class Metrics(object):
         :return: (float) two_point_percentage
         """
         return round(float(two_point_goals) / two_point_attempts, 3)
+
+    @classmethod
+    def effective_field_goal_percentage(cls, field_goals, three_point_field_goals, field_goal_attempts):
+        """
+        Effective Field Goal Percentage; the formula is (FG + 0.5 * 3P) / FGA.
+        :param field_goals: Field Goals
+        :param three_point_field_goals: 3-Point Field Goals
+        :param field_goal_attempts: Field Goal Attempts
+        :return: (float) effective_field_goal_percentage
+        """
+        return round((float(field_goals) + 0.5 * three_point_field_goals) / field_goal_attempts, 3)
+
+    @classmethod
+    def free_throws_per_game(cls, free_throws, games):
+        """
+        Free Throws Per Game
+        :param free_throws: Free Throws
+        :param games: Games
+        :return: (float) free_throws_per_game
+        """
+        return round(float(free_throws) / games, 1)
+
+    @classmethod
+    def free_throw_attempts_per_game(cls, free_throw_attempts, games):
+        """
+        Free Throw Attempts Per Game
+        :param free_throw_attempts: Free Throw Attempts
+        :param games: Games
+        :return: (float) free_throw_attempts_per_game
+        """
+        return round(float(free_throw_attempts) / games, 1)
+
+    @classmethod
+    def free_throw_percentage(cls, free_throws, free_throw_attempts):
+        """
+        Free Throw Percentage; the formula is FT / FTA.
+        :param free_throws: Free Throws
+        :param free_throw_attempts: Free Throw Attempts
+        :return: (float) free_throw_percentage
+        """
+        return round(float(free_throws) / free_throw_attempts, 3)
+
+    @classmethod
+    def offensive_rebounds_per_game(cls, offensive_rebounds, games):
+        """
+        Offensive Rebounds Per Game
+        :param offensive_rebounds: Offensive Rebounds
+        :param games: Games
+        :return: (float) offensive_rebounds_per_game
+        """
+        return round(float(offensive_rebounds) / games, 1)
+
+    @classmethod
+    def deffensive_rebounds_per_game(cls, deffensive_rebounds, games):
+        """
+        Deffensive Rebounds Per Game
+        :param deffensive_rebounds: Deffensive Rebounds
+        :param games: Games
+        :return: (float) deffensive_rebounds_per_game
+        """
+        return round(float(deffensive_rebounds) / games, 1)
+
+    @classmethod
+    def total_rebounds_per_game(cls, total_rebounds, games):
+         """
+         Total Rebounds Per Game
+         :param total_rebounds: Total Rebounds
+         :param games: Games
+         :return: (float) total_rebounds_per_game
+         """
+         return round(float(total_rebounds) / games, 1)
--- tests/apbrmetrics/test_metrics.py
+++ tests/apbrmetrics/test_metrics.py
@@ -65,3 +65,33 @@ class TestMetrics(unittest.TestCase):
         # Lebron James(2003-2004)
         two_point_field_goal_percentage = Metrics.two_point_field_goal_percentage(559, 1275)
         self.assertEqual(two_point_field_goal_percentage, 0.438)
+
+    def test_effective_field_goal_percentage(self):
+        # Lebron James(2003-2004)
+        effective_field_goal_percentage = Metrics.effective_field_goal_percentage(622, 63, 1492)
+        self.assertEqual(effective_field_goal_percentage, 0.438)
+
+    def test_free_throws_per_game(self):
+        # Lebron James(2003-2004)
+        free_throws_per_game = Metrics.free_throws_per_game(347, 79)
+        self.assertEqual(free_throws_per_game, 4.4)
+
+    def test_free_throw_attempts_per_game(self):
+        # Lebron James(2003-2004)
+        free_throw_attempts_per_game = Metrics.free_throw_attempts_per_game(460, 79)
+        self.assertEqual(free_throw_attempts_per_game, 5.8)
+
+    def test_free_throw_percentage(self):
+        # Lebron James(2003-2004)
+        free_throw_percentage = Metrics.free_throw_percentage(347, 460)
+        self.assertEqual(free_throw_percentage, 0.754)
+
+    def test_offensive_rebounds_per_game(self):
+        # Lebron James(2003-2004)
+        offensive_rebounds_per_game = Metrics.offensive_rebounds_per_game(99, 79)
+        self.assertEqual(offensive_rebounds_per_game, 1.3)
+
+    def test_deffensive_rebounds_per_game(self):
+        # Lebron James(2003-2004)
+        deffensive_rebounds_per_game = Metrics.deffensive_rebounds_per_game(333, 79)
+        self.assertEqual(deffensive_rebounds_per_game, 4.2)
